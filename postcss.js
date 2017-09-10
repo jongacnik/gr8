0786ddaa4bbdca0554e950c2d15bee8e6070e809
Jon Gacnik <jon@folderstudio.com>
2017-09-09 19:57:51 -0700

    postcss plugin

---
+++ postcss.js
@@ -0,0 +1,35 @@
+var postcss = require('postcss')
+var gr8util = require('gr8-util')
+var gr8utils = require('./utils')
+var gr8 = require('.')
+ 
+module.exports = postcss.plugin('gr8', function (options) {
+  return function (css) {
+    css.walkAtRules(function (atRule) {
+      if (['gr8', 'gr8utils', 'gr8util'].indexOf(atRule.name) < 0) {
+        return;
+      }
+
+      var input = atRule.raws.between
+
+      // remove comment operators and newlines
+      var clean = input.replace(/(\/\*|\*\/|\n)/g, '')
+      
+      // eval string, it better be valid js!
+      var options = clean ? eval(`(${clean})`) : {}
+      
+      // create utils from options
+      if (atRule.name === 'gr8') {
+        var css = gr8(options)
+      } else if (atRule.name === 'gr8utils') {
+        var css = gr8utils(options)
+      } else if (atRule.name === 'gr8util') {
+        var css = gr8util(options)
+      }
+
+      // insert utils
+      atRule.parent.insertBefore(atRule, css)
+      atRule.remove()
+    })
+  }
+})
