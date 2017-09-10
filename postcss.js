413b68e0f4f9264b0a4dd3c75baa86dea2fa1d77
Jon Gacnik <jon@folderstudio.com>
2017-09-09 20:14:21 -0700

    standard

--- postcss.js
+++ postcss.js
@@ -2,29 +2,30 @@ var postcss = require('postcss')
 var gr8util = require('gr8-util')
 var gr8utils = require('./utils')
 var gr8 = require('.')
- 
+
 module.exports = postcss.plugin('gr8', function (options) {
   return function (css) {
     css.walkAtRules(function (atRule) {
       if (['gr8', 'gr8utils', 'gr8util'].indexOf(atRule.name) < 0) {
-        return;
+        return
       }
 
       var input = atRule.raws.between
 
       // remove comment operators and newlines
       var clean = input.replace(/(\/\*|\*\/|\n)/g, '')
-      
+
       // eval string, it better be valid js!
       var options = clean ? eval(`(${clean})`) : {}
-      
+
       // create utils from options
+      var css = ''
       if (atRule.name === 'gr8') {
-        var css = gr8(options)
+        css = gr8(options)
       } else if (atRule.name === 'gr8utils') {
-        var css = gr8utils(options)
+        css = gr8utils(options)
       } else if (atRule.name === 'gr8util') {
-        var css = gr8util(options)
+        css = gr8util(options)
       }
 
       // insert utils
