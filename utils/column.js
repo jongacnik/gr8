9f877c4b962c4feac6293e66131702523b1c6aa3
Seth Thompson <s3th.thompson@gmail.com>
2018-01-07 13:54:27 -0800

    increase column width precision to 5 decimal places

--- utils/column.js
+++ utils/column.js
@@ -3,7 +3,7 @@ exports.column = {
     c: 'width'
   },
   vals: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-  transform: v => Math.round((v / 12 * 100) * 100) / 100,
+  transform: v => Math.round((v / 12 * 100) * 100000) / 100000,
   unit: '%'
 }
 
@@ -12,7 +12,7 @@ exports.split = {
     s: 'width'
   },
   vals: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-  transform: v => Math.round((1 / v * 100) * 100) / 100,
+  transform: v => Math.round((1 / v * 100) * 100000) / 100000,
   unit: '%'
 }
 
@@ -21,6 +21,6 @@ exports.offset = {
     co: 'margin-left'
   },
   vals: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-  transform: v => Math.round((v / 12 * 100) * 100) / 100,
+  transform: v => Math.round((v / 12 * 100) * 100000) / 100000,
   unit: '%'
 }
